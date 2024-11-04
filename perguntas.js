f4d026e6c0a3f1040aaf644f2dfc01ead27c15f4
Adrian Fernando Damasio <00001105501334sp@al.educacao.sp.gov.br>
2024-11-04 10:44:50 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Desenvolvimento de Sistema',
+    'Para que serve o C#?',
+    'Com C# é possível desenvolver aplicações de praticamente todos os tipos: desde aplicações embarcadas até aplicativos de área de trabalho, mobile e sistemas web.'
+)
+
+criaCartao(
+    'Banco de Dados',
+    'qual o significado de C,P,F?',
+    'CPF é sigla para Cadastro de Pessoa Física. '
+)
+
+criaCartao(
+    '<html> vs.code.dev',
+    'Oque é Visiul Studio Code?',
+    ' Visual Studio é uma plataforma de lançamento criativa que você pode usar para editar, depurar e criar código e, em seguida, publicar um aplicativo.'
+)
+
+criaCartao(
+    'Linux',
+    'Oque é linux?',
+    'O Linux é um sistema operacional para computadores desenvolvido a partir de um modelo de código aberto.'
+)
